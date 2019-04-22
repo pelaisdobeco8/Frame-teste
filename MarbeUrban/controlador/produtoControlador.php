0b6cdf2022752535e934d29137e1a1df2ccf77d0
Bea <47746451+pelaisdobeco8@users.noreply.github.com>
2019-04-22 10:50:09 -0300

    Add files via upload
    minha pagina

---
+++ MarbeUrban/controlador/produtoControlador.php
@@ -0,0 +1,10 @@
+<?php
+
+function visualizar() {
+	$vetor = array();
+	$vetor['produto'] = "Moletom";
+	$vetor['descricao'] = "Rosa Marfim";
+	$vetor['preco'] = "159,90";
+	
+    exibir("paginas/visualizar");
+}
---
+++ MarbeUrban/visao/paginas/visualizar.visao.php
@@ -0,0 +1,4 @@
+<h3>DADOS:</h3>
+<p>Nome do Produto: <?=$visao['produto']?></p>
+<p>Descrição: <?=$visao['descricao']?></p>
+<p>Preço: <?=$visao['preco']?></p>
